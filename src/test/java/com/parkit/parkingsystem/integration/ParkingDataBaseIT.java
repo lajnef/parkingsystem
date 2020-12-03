36765b7e423cfaee27ca5c85b464eef28c669877
LAJNEF <maroua.lajnef@allianz.fr>
2020-12-03 11:26:39 +0100

    add mariadb driver

--- src/test/java/com/parkit/parkingsystem/integration/ParkingDataBaseIT.java
+++ src/test/java/com/parkit/parkingsystem/integration/ParkingDataBaseIT.java
@@ -14,6 +14,7 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import static org.mockito.Mockito.when;
+import static org.junit.jupiter.api.Assertions.*;
 
 @ExtendWith(MockitoExtension.class)
 public class ParkingDataBaseIT {
@@ -51,6 +52,7 @@ public class ParkingDataBaseIT {
     public void testParkingACar(){
         ParkingService parkingService = new ParkingService(inputReaderUtil, parkingSpotDAO, ticketDAO);
         parkingService.processIncomingVehicle();
+       assertNotNull(ticketDAO.getTicket("ABCDEF"));
         //TODO: check that a ticket is actualy saved in DB and Parking table is updated with availability
     }
 
